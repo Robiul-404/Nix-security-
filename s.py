92f1e99274b7872211fcfc196b2bf7797fe6ecbc
Robiul-404 <130835737+Robiul-404@users.noreply.github.com>
2024-05-26 22:56:46 +0600

    Add files via upload

--- s.py
+++ s.py
@@ -1,72 +1,49 @@
 logo=(f"""
 
 
-
-\033[1;31;40m        `7MN.   `7MF'`7MMF'`YMM'   `MP' 
-          MMN.    M    MM    VMb.  ,P   
-          M YMb   M    MM     `MM.M'    
-          M  `MN. M    MM       MMb     
-          M   `MM.M    MM     ,M'`Mb.   
-          M     YMM    MM    ,P   `MM.  
-        .JML.    YM  .JMML..MM:.  .:MMa.        
-\033[1;92m__________________________________________________
-
- \033[1;34m   ▁ ▂ ▃ ▅ ▆ ▇ █ \033[1;37mAUTHOR:MR:NIX\033[1;34m █ ▇ ▆ ▅ ▃ ▂ ▁                                                         
-\033[1;92m──────────────────────────────────────────────────
-\033[1;92m Owner   :            MR:NIX
-\033[1;92m Facebook:            Mr:Nix
-\033[1;92m Github  :            Mr-NIX8
-\033[1;92m Version :          \033[1;33;40m  0.1.1
-\033[1;92m──────────────────────────────────────────────────
-
-None
-|+| TOTAL ID : 99999
-|+| SIM CODE   : +91639
-|+| USE AIRPLANE MODE EVERY 5 MINTS
-──────────────────────────────────────────────────
-\033[1;92m|NIX-OK🥰| 100077603523047 | 56173195
+\033[1;32m
+███    ███ ██████     ███    ██ ██ ██   ██        ████  ████ ██   ██    ████   ██ ██  ██ ██
+██ ████ ██ ██████     ██ ██  ██ ██   ███          ██  ██  ██ ██   ██    ██  ██ ██ ██  ██ ██         ██      ██ ██   ██ ██ ██   ████ ██ ██   ██ V/0.4.1
+\x1b[1;33m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━     \033[1;97m[✓] TOTAL IDS  :\033[1;92m 9999   \033[1;97m                          [✓] YOUR CLONNIG STARTD......
+───────────────────────────────────────────────   
+\033[1;92m[NIX-OK] 100077603523047 | 56173195
 \033[1;37m|COOKIE🍪|> c_user=100077603523047;xs=11:g7dFL8baGLi6JA:2:1708619251:-1:5241;fr=08jmLPUL9nZdBc6pg.AWXnYem1aHlI-mXyntKRSS_ma3g.Bl13Xx..AAA.0.0.Bl13Xx.AWVpnhN7OcU;datr=8XXXZWqPFuatj6V1GdVKZ6Js
-\033[30;1m|NIX-CP😔| 100096496475858 | 55885855
-\033[1;92m|NIX-OK🥰| 100055460131977 | 57273200
+\033[1;92m[NIX-OK] 100055460131977 | 57273200
 \033[1;37m|COOKIE🍪|> c_user=100055460131977;xs=21:NLsg2bNoxo1eLg:2:1708619670:-1:13723;fr=046EF1RN7HQvjlrRK.AWV5FS8MyYE1xJ50YCFpiK7OvWY.Bl13eV..AAA.0.0.Bl13eV.AWXqayY-684;datr=lXfXZSsac88VFxtA8ANsQGST
-\033[1;92m|NIX-OK🥰| 100024871161085 | 0608257
+\033[1;92m[NIX-OK] 100024871161085 | 0608257
 \033[1;37m|COOKIE🍪|> c_user=100024871161085;xs=18:0CLl-3OmqrAhaw:2:1708619693:-1:5290;fr=0BNpadzy8sZo82bTi.AWVUxYFrxbNdzB1YUlcUPyzUn3c.Bl13et..AAA.0.0.Bl13et.AWXwZgmcLtw;datr=rXfXZRd5S6q4XsSMPtMQfRa7
-\033[30;1m|NIX-CP😔| 100008548676868 | 955875555
-\033[1;92m|NIX-OK🥰| 100077603523047 | 56173195
+\033[1;92m[NIX-OK] 100077603523047 | 56173195
 \033[1;37m|COOKIE🍪|> c_user=100077603523047;xs=11:g7dFL8baGLi6JA:2:1708619251:-1:5241;fr=08jmLPUL9nZdBc6pg.AWXnYem1aHlI-mXyntKRSS_ma3g.Bl13Xx..AAA.0.0.Bl13Xx.AWVpnhN7OcU;datr=8XXXZWqPFuatj6V1GdVKZ6Js
-\033[1;92m|NIX-OK🥰| 100055460131977 | 57273200
+\033[1;92m[NIX-OK] 100055460131977 | 57273200
 \033[1;37m|COOKIE🍪|> c_user=100055460131977;xs=21:NLsg2bNoxo1eLg:2:1708619670:-1:13723;fr=046EF1RN7HQvjlrRK.AWV5FS8MyYE1xJ50YCFpiK7OvWY.Bl13eV..AAA.0.0.Bl13eV.AWXqayY-684;datr=lXfXZSsac88VFxtA8ANsQGST
-\033[1;92m|NIX-OK🥰| 100024871161085 | 018257
+\033[1;92m[NIX-OK] 100024871161085 | 018257
 \033[1;37m|COOKIE🍪|> c_user=100024871161085;xs=18:0CLl-3OmqrAhaw:2:1708619693:-1:5290;fr=0BNpadzy8sZo82bTi.AWVUxYFrxbNdzB1YUlcUPyzUn3c.Bl13et..AAA.0.0.Bl13et.AWXwZgmcLtw;datr=rXfXZRd5S6q4XsSMPtMQfRa7
-\033[1;92m|NIX-OK🥰| 100045603523047 | 5615195
+\033[1;92m[NIX-OK] 100045603523047 | 5615195
 \033[1;37m|COOKIE🍪|> c_user=100045603523047;xs=11:g7dFL8baGLi6JA:2:1708619251:-1:5241;fr=08jmLPUL9nZdBc6pg.AWXnYem1aHlI-mXyntKRSS_ma3g.Bl13Xx..AAA.0.0.Bl13Xx.AWVpnhN7OcU;datr=8XXXZWqPFuatj6V1GdVKZ6Js
-\033[30;1m|NIX-CP😔| 100087642523479 | 57273200
-\033[1;92m|NIX-OK🥰| 100055460131977 | 57273200
+\033[1;92m[NIX-OK] 100055460131977 | 57273200
 \033[1;37m|COOKIE🍪|> c_user=100055460131977;xs=21:NLsg2bNoxo1eLg:2:1708619670:-1:13723;fr=046EF1RN7HQvjlrRK.AWV5FS8MyYE1xJ50YCFpiK7OvWY.Bl13eV..AAA.0.0.Bl13eV.AWXqayY-684;datr=lXfXZSsac88VFxtA8ANsQGST
-\033[1;92m|NIX-OK🥰| 100024846161085 | 019546
+\033[1;92m[NIX-OK] 100024846161085 | 019546
 \033[1;37m|COOKIE🍪|> c_user=100024846161085;xs=18:0CLl-3OmqrAhaw:2:1708619693:-1:5290;fr=0BNpadzy8sZo82bTi.AWVUxYFrxbNdzB1YUlcUPyzUn3c.Bl13et..AAA.0.0.Bl13et.AWXwZgmcLtw;datr=rXfXZRd5S6q4XsSMPtMQfRa7
-\033[1;92m|NIX-OK🥰| 100092828776867 | 56173195
+\033[1;92m[NIX-OK] 100092828776867 | 56173195
 \033[1;37m|COOKIE🍪|> c_user=100092828776867;xs=11:g7dFL8baGLi6JA:2:1708619251:-1:5241;fr=08jmLPUL9nZdBc6pg.AWXnYem1aHlI-mXyntKRSS_ma3g.Bl13Xx..AAA.0.0.Bl13Xx.AWVpnhN7OcU;datr=8XXXZWqPFuatj6V1GdVKZ6Js
-\033[30;1m|NIX-CP😔| 100071868658676 | 55885855
-\033[1;92m|NIX-OK🥰| 100055460131977 | 57273200
+\033[1;92m[NIX-OK] 100055460131977 | 57273200
 \033[1;37m|COOKIE🍪|> c_user=100055460131977;xs=21:NLsg2bNoxo1eLg:2:1708619670:-1:13723;fr=046EF1RN7HQvjlrRK.AWV5FS8MyYE1xJ50YCFpiK7OvWY.Bl13eV..AAA.0.0.Bl13eV.AWXqayY-684;datr=lXfXZSsac88VFxtA8ANsQGST
-\033[1;92m|NIX-OK🥰| 100086585375375 | 0608257
+\033[1;92m[NIX-OK] 100086585375375 | 0608257
 \033[1;37m|COOKIE🍪|> c_user=100086585375375;xs=18:0CLl-3OmqrAhaw:2:1708619693:-1:5290;fr=0BNpadzy8sZo82bTi.AWVUxYFrxbNdzB1YUlcUPyzUn3c.Bl13et..AAA.0.0.Bl13et.AWXwZgmcLtw;datr=rXfXZRd5S6q4XsSMPtMQfRa7
-\033[30;1m|NIX-CP😔| 100081975986986 | 955875555
-\033[1;92m|NIX-OK🥰| 100077603523047 | 56173195
+\033[1;92m[NIX-OK] 100077603523047 | 56173195
 \033[1;37m|COOKIE🍪|> c_user=100077603523047;xs=11:g7dFL8baGLi6JA:2:1708619251:-1:5241;fr=08jmLPUL9nZdBc6pg.AWXnYem1aHlI-mXyntKRSS_ma3g.Bl13Xx..AAA.0.0.Bl13Xx.AWVpnhN7OcU;datr=8XXXZWqPFuatj6V1GdVKZ6Js
-\033[1;92m|NIX-OK🥰| 100086437647908 | 57273200
+\033[1;92m[NIX-OK] 100086437647908 | 57273200
 \033[1;37m|COOKIE🍪|> c_user=100086437647908;xs=21:NLsg2bNoxo1eLg:2:1708619670:-1:13723;fr=046EF1RN7HQvjlrRK.AWV5FS8MyYE1xJ50YCFpiK7OvWY.Bl13eV..AAA.0.0.Bl13eV.AWXqayY-684;datr=lXfXZSsac88VFxtA8ANsQGST
-\033[1;92m|NIX-OK🥰| 100025621458078 | 018257
+\033[1;92m[NIX-OK] 100025621458078 | 018257
 \033[1;37m|COOKIE🍪|> c_user=100025621458078;xs=18:0CLl-3OmqrAhaw:2:1708619693:-1:5290;fr=0BNpadzy8sZo82bTi.AWVUxYFrxbNdzB1YUlcUPyzUn3c.Bl13et..AAA.0.0.Bl13et.AWXwZgmcLtw;datr=rXfXZRd5S6q4XsSMPtMQfRa7
-\033[1;92m|NIX-OK🥰| 100065413365878 | 5615195
+\033[1;92m[NIX-OK] 100065413365878 | 5615195
 \033[1;37m|COOKIE🍪|> c_user=100065413365878;xs=11:g7dFL8baGLi6JA:2:1708619251:-1:5241;fr=08jmLPUL9nZdBc6pg.AWXnYem1aHlI-mXyntKRSS_ma3g.Bl13Xx..AAA.0.0.Bl13Xx.AWVpnhN7OcU;datr=8XXXZWqPFuatj6V1GdVKZ6Js
 \033[30;1m|NIX-CP😔| 100054138742584 | 57273200
-\033[1;92m|NIX-OK🥰| 100055284156042 | 57273200
+\033[1;92m[NIX-OK] 100055284156042 | 57273200
 \033[1;37m|COOKIE🍪|> c_user=100055284156042;xs=21:NLsg2bNoxo1eLg:2:1708619670:-1:13723;fr=046EF1RN7HQvjlrRK.AWV5FS8MyYE1xJ50YCFpiK7OvWY.Bl13eV..AAA.0.0.Bl13eV.AWXqayY-684;datr=lXfXZSsac88VFxtA8ANsQGST
-\033[1;92m|NIX-OK🥰| 100054258855885 | 019546
+\033[1;92m[NIX-OK] 100054258855885 | 019546
 \033[1;37m|COOKIE🍪|> c_user=100054258855885;xs=18:0CLl-3OmqrAhaw:2:1708619693:-1:5290;fr=0BNpadzy8sZo82bTi.AWVUxYFrxbNdzB1YUlcUPyzUn3c.Bl13et..AAA.0.0.Bl13et.AWXwZgmcLtw;datr=rXfXZRd5S6q4XsSMPtMQfRa7
-\033[1;92m|NIX-M1| 97586 18|6
+\033[1;97m[NIX-XD] 0|9999|M1 \033[1;92m[OK:-0]
 
 
 
